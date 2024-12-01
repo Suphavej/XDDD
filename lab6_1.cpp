7435215a531b77e99230011a6963ea7212725bda
Suphavej <fatkimmmm@gmail.com>
2024-12-01 23:22:21 +0700

    lab6_1.cpp completed

--- lab6_1.cpp
+++ lab6_1.cpp
@@ -2,8 +2,24 @@
 using namespace std;
 
 int main(){
+int a[]={1};
+int i=0,x=0,y=0;
+
+while (a[i] != 0){
     cout << "Enter an integer: ";
-    cout << "#Even numbers = ";
-    cout << "#Odd numbers = ";
-    return 0;
+    cin >> a[i];
+    if(a[i] == 0){
+        break;
+    }
+    if(a[i] % 2 == 0){
+        x++;
+    }
+    else{
+        y++;
+    }
+}
+    
+cout << "#Even numbers = " <<x;
+cout << "\n#Odd numbers = " <<y;
+return 0;
 }
